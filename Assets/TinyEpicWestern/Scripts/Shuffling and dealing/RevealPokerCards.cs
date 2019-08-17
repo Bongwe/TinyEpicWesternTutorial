50a3e9498579f25603e83a0deecc85c80fb9f990
bongwe <obomvu@gmail.com>
2019-08-17 14:57:05 +0200

    Added animation for revealing poker cards

---
+++ Assets/TinyEpicWestern/Scripts/Shuffling and dealing/RevealPokerCards.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealPokerCards : MonoBehaviour
+{
+    public GameObject[] pokerCards;
+
+    private float currentTime = 0;
+    private float targetTime = 1;
+    private int index = 0;
+    private bool revealCards = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = currentTime + Time.deltaTime;
+
+        if (currentTime >= targetTime && index < pokerCards.Length && revealCards == true)
+        {
+            pokerCards[index].SetActive(true);
+            currentTime = 0;
+            index++;
+            if (index >= pokerCards.Length)
+            {
+                revealCards = false;
+                index = 0;
+            }
+        }
+
+        if (currentTime >= targetTime && index <= pokerCards.Length && revealCards == false)
+        {
+            pokerCards[index].SetActive(false);
+            currentTime = 0;
+            index++;
+            if (index == pokerCards.Length)
+            {
+                revealCards = true;
+                index = 0;
+            }
+        }
+
+    }
+}
