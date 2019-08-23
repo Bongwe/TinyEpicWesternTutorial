001f3d85b52d91022906827c6cae375cb5f548eb
bongwe <obomvu@gmail.com>
2019-08-23 22:21:34 +0200

    Completed Shuffling and dealing

---
+++ Assets/TinyEpicWestern/Scripts/EditOnEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditOnEvent : MonoBehaviour
+{
+    public int delayTime = 5;
+    private float currentTime = 0;
+    private bool updateTime = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (updateTime)
+        {
+            currentTime = currentTime + Time.deltaTime;
+            if (currentTime >= delayTime)
+            {
+                gameObject.SetActive(false);
+                updateTime = false;
+            }
+        }
+    }
+}
