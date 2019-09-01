2fe79fa2bca7514524960211e7e81106239311b8
bongwe <obomvu@gmail.com>
2019-09-01 21:48:09 +0200

    Working on gameplay menu

---
+++ Assets/TinyEpicWestern/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource buttonClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void gameplayScene()
+    {
+        buttonClick.Play();
+        SceneManager.LoadSceneAsync("1.1 - Menu Gameplay");
+    }
+
+    public void mainManeScene()
+    {
+        buttonClick.Play();
+        SceneManager.LoadSceneAsync("1.0 Menu");
+    }
+
+}
